0e2b65c54c7f605f4d49cd6509d88be91e24ae10
Jesse Qin <dev@jq.run>
2023-11-30 20:22:24 -0500

    Prepare for 2023

---
+++ 2023/typescript/.eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+      },
+    ],
+    'no-constant-condition': 0,
+  },
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+};
